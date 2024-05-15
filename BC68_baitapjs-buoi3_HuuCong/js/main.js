bf343309b3e0b7327e861935e336004e8a2b3d96
CongKTI <congkti@gmail.com>
2024-05-15 18:53:59 +0700

    push code

--- BC68_baitapjs-buoi3_HuuCong/js/main.js
+++ BC68_baitapjs-buoi3_HuuCong/js/main.js
@@ -47,20 +47,20 @@ function xoaForm(elInput, elOutput) {
  *
  *
  * QUÁ TRÌNH XỬ LÝ:
- * - DOM đến các thẻ tương ứng để lấy dữ liệu số ngày làm và Lương căn bản ngày
+ * - DOM để lấy đối tượng lấy dữ liệu số ngày làm, Lương căn bản 1 ngày
  * - DOM và gắn sự kiện cho nút Tính lương và tính lương thực tế
- * - Kiểm tra dữ liệu nhập vào có phải number? Nếu bỏ trống hoặc nhập không phải number sẽ thông báo + xóa để nhập lại
+ * - Kiểm tra dữ liệu nhập vào có phải number hoặc chưa nhập? Nếu bỏ trống hoặc nhập không phải number sẽ thông báo + xóa để nhập lại
  * - Lương thực tế = Lương căn bản ngày x Số ngày làm
  * - DOM đến thẻ kết quả để hiển thị kết quả Lương thực tế
  *
  *
- * ĐẦU RA: Hiển thị lượng thực tế nhận được của nhân viên trên giao diện
+ * ĐẦU RA: Hiển thị lương thực tế nhận được của nhân viên trên giao diện
  *
  */
 let luongCB = document.getElementById("luongCanBan");
 let soNgayLam = document.getElementById("soNgayLam");
 let hienThiTinhLuong = document.querySelector("#v-pills-bai01 .result .salary");
-console.log(hienThiTinhLuong);
+// console.log(hienThiTinhLuong);
 
 document.getElementById("btnTinhTienLuong").onclick = () => {
   if (!soNgayLam.value || isNaN(soNgayLam.value)) {
@@ -70,7 +70,7 @@ document.getElementById("btnTinhTienLuong").onclick = () => {
     xoaForm("#soNgayLam", "#v-pills-bai01 .result");
   } else {
     let luongThucNhan = luongCB.value * soNgayLam.value;
-    console.log(luongThucNhan);
+    // console.log(luongThucNhan);
     document.querySelector("#v-pills-bai01 .result").style.opacity = "1";
     hienThiTinhLuong.innerHTML = luongThucNhan.toLocaleString("vi-VN", {
       style: "currency",
@@ -86,31 +86,43 @@ document.getElementById("btnTinhTienLuong").onclick = () => {
  *
  *
  * QUÁ TRÌNH XỬ LÝ:
- * - DOM đến các thẻ tương ứng để lấy dữ liệu 5 số số thực đã nhập
+ * - Viết hàm để kiểm tra dữ liệu nhậm vào có phải Number? Validate thông báo khi user đang nhập bằng sự kiện .onChange
  * - DOM và gắn sự kiện cho nút Tính giá trị trung bình
- * - Kiểm tra ép đúng kiểu dữ liệu số mới thực hiện tính toán
- * - Giá trị trung bình = Tổng 5 số / 5
- * - DOM đến thẻ kết quả để hiển thị kết quả Lương thực tế
+ * - DOM đến các thẻ tương ứng để lấy dữ liệu 5 số số thực đã nhập
+ * - Kiểm tra loại bỏ TH không nhập cả 5 số
+ * - Tính Giá trị trung bình = Tổng 5 số / 5
+ * - DOM đến thẻ kết quả để hiển thị kết quả Giá trị TB
  *
  *
- * ĐẦU RA: Hiển thị lượng thực tế nhận được của nhân viên trên giao diện
+ * ĐẦU RA: Hiển thị Giá trị trung bình của 5 số trên giao diện
  */
 function checkInputNumber(inputID) {
   let input = document.getElementById(inputID);
   if (!input.value || isNaN(input.value)) {
     alert(
-      `Đang nhập giá trị = ${input.value}. Vui lòng nhập số thực và không được để trống!!`
+      `Đang nhập giá trị = ${input.value}. Vui lòng nhập số thực và không được để trống. Nếu để trống sẽ tính với giá trị bằng 0 !!!`
     );
-    xoaForm("#" + input, "");
+    input.value = "";
+    input.focus();
   }
 }
-let soThuNhat = document.getElementById("soThuNhat");
-let soThuHai = document.getElementById("soThuHai");
-let soThuBa = document.getElementById("soThuBa");
-let soThuTu = document.getElementById("soThuTu");
-let soThuNam = document.getElementById("soThuNam");
-let hienThiTinhTrungBinh = document.querySelector(
-  "#v-pills-bai02 .result .salary"
-);
 
-document.getElementById("btnTinhTrungBinh").onclick = () => {};
+document.getElementById("btnTinhTrungBinh").onclick = () => {
+  let soThuNhat = document.getElementById("soThuNhat").value * 1;
+  let soThuHai = document.getElementById("soThuHai").value * 1;
+  let soThuBa = document.getElementById("soThuBa").value * 1;
+  let soThuTu = document.getElementById("soThuTu").value * 1;
+  let soThuNam = document.getElementById("soThuNam").value * 1;
+  let hienThiTinhTrungBinh = document.querySelector(
+    "#v-pills-bai02 .result .salary"
+  );
+  let tongNamSo = soThuNhat + soThuHai + soThuBa + soThuTu + soThuNam;
+  if (tongNamSo == 0) {
+    alert("Chưa nhập số nào mà tính gì?");
+    document.getElementById("soThuNhat").focus();
+  } else {
+    let giaTriTrungBinh = tongNamSo / 5;
+    document.querySelector("#v-pills-bai02 .result").style.opacity = "1";
+    hienThiTinhTrungBinh.innerHTML = giaTriTrungBinh.toLocaleString("vi-VN");
+  }
+};
